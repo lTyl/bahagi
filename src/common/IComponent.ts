e3094c6ff3afafd8fad673ba39d9be028dd62869
Tyler <tyler@microtekcorporation.com>
2018-07-29 13:16:27 -0600

    Update IComponent typingz

--- src/common/IComponent.ts
+++ src/common/IComponent.ts
@@ -2,7 +2,7 @@
  * Interface for a component containing both mandatory and optional properties.
  */
 export interface IComponent<T> {
-	new (...args: any[]): T;
+	(...args: any[]): T;
 	active?: any;
 	tag?: any;
 	update?: any;
